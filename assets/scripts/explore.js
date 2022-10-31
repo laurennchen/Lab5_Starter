a5d1413fca638c96ba76c3334b0f83458d335b64
laurennchen <lmchen@ucsd.edu>
2022-10-31 14:27:23 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,58 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // SET LIST OF VOICE OPTIONS
+  const synth = window.speechSynthesis;
+  let voices = [];
+  function populateVoiceList() {
+    if (typeof speechSynthesis === 'undefined') {
+      return;
+    }
+  
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // SPEAK WHEN BUTTON IS PRESSED
+  const button = document.querySelector('button');
+  const voiceSelect = document.querySelector('select');
+  button.addEventListener('click', (event) => {
+    const text = document.getElementById("text-to-speak").value;
+    const speakText = new SpeechSynthesisUtterance(text);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        speakText.voice = voices[i];
+      }
+    }
+    synth.speak(speakText);
+    
+  })
+
+  var isSpeaking = synth.speaking;
+  console.log(isSpeaking);
+   
+  while (isSpeaking == true) {
+    isSpeaking = synth.speaking;
+    console.log(isSpeaking);
+  }
+
 }
